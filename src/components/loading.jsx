0d20b3652fc14c1f76fe77740a8403d80b097b79
Jean-Marc Da Pozzo <jmdapozzo@macdap.com>
2023-01-18 15:57:19 -0500

    Feature: improved loading page visual

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import { Container, Row } from "react-bootstrap";
 import Spinner from "react-bootstrap/Spinner";
 
-class Loading extends React.Component {
-  render() {
-    return (
-      <div className="spinner">
-        <Spinner animation="border" />
-      </div>
-    );
-  }
+function Loading() {
+  const { t } = useTranslation(["common"]);
+  return (
+    <Container fluid className="p-4">
+      <Row className="pb-2 d-flex justify-content-center">{t("loading")}</Row>
+      <Row className="d-flex justify-content-center">
+        <Spinner />
+      </Row>
+    </Container>
+  );
 }
 
 export default Loading;
