6302c88b0f4f37ab93056a2155a24dc21e706708
Jean-Marc Da Pozzo <jmdapozzo@macdap.com>
2023-04-21 18:18:45 -0400

    Feature: device edition dialog for setting owner information and setting or clearing device version lock

--- src/components/device-page.jsx
+++ src/components/device-page.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { useAuth0 } from "@auth0/auth0-react";
 import Loading from "./loading";
 import { Container, Row, Col, Alert } from "react-bootstrap";
 import DeviceTable from "./device-table";
 
 function DevicePage(props) {
+  const { t } = useTranslation(["device", "common"]);
+
   const [devices, setDevices] = useState([]);
   const [result, setResult] = useState({ hasError: false });
 
@@ -35,6 +38,86 @@ function DevicePage(props) {
     getDevices();
   }, [getAccessTokenSilently]);
 
+  const updateDeviceOwner = async (device) => {
+
+    let bodyData = {
+      device_owner_id: device.device_owner_id,
+      company_name: device.company_name,
+      contact_name: device.contact_name,
+      contact_email: device.contact_email,
+      location_name: device.location_name
+    };
+
+    try {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch(
+        process.env.REACT_APP_SERVER_ENDPOINT + "/device/v2/owner",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(bodyData),
+        }
+      );
+
+      const responseData = await response.json();
+      if (responseData.putOwner === true) {
+        const deviceIndex = devices.findIndex((data) => data.device_id === device.device_id);
+        const newDeviceArray = [
+          ...devices.slice(0, deviceIndex),
+          device,
+          ...devices.slice(deviceIndex + 1),
+        ];
+        setDevices(newDeviceArray);
+      }else{
+        setResult({ hasError: true, message: t("device:errorMsg.cannotUpdateDevice") });
+      }
+    } catch (error) {
+      setResult({ hasError: true, message: error.message });
+    }
+  };
+
+  const updateDeviceLockVersion = async (device) => {
+    let bodyData = {
+      device_application_id: device.device_application_id,
+      lock_version: device.lock_version,
+    };
+
+    try {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch(
+        process.env.REACT_APP_SERVER_ENDPOINT + "/device/v2/lock-version",
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(bodyData),
+        }
+      );
+
+      const responseData = await response.json();
+      if (responseData.putLockVersion === true) {
+        const deviceIndex = devices.findIndex((data) => data.device_id === device.device_id);
+        const newDeviceArray = [
+          ...devices.slice(0, deviceIndex),
+          device,
+          ...devices.slice(deviceIndex + 1),
+        ];
+        setDevices(newDeviceArray);
+      }else{
+        setResult({ hasError: true, message: t("device:errorMsg.cannotChangeLockStatus") });
+      }
+    } catch (error) {
+      setResult({ hasError: true, message: error.message });
+    }
+  };
+
   if (isLoading) {
     <Loading />;
   }
@@ -44,7 +127,11 @@ function DevicePage(props) {
       <Row>
         <Col>
           {!result.hasError ? (
-            <DeviceTable devices={devices} />
+            <DeviceTable
+              devices={devices}
+              updateDeviceOwner={updateDeviceOwner}
+              updateDeviceLockVersion={updateDeviceLockVersion}
+            />
           ) : (
             <Alert variant="danger"> {result.message} </Alert>
           )}
--- src/components/device-table.jsx
+++ src/components/device-table.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Table } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 import moment from "moment";
 import "moment/locale/fr";
 import DeviceForm from "./device-form";
 
 const formatOwner = (company, contact, email) => {
-  if ((company) && (contact)) {
+  if (company && contact) {
     return `${company} (${contact})`;
-  } else if ((!company) && (contact)) {
+  } else if (!company && contact) {
     return `${contact}`;
-  } else if ((company) && (!contact)) {
+  } else if (company && !contact) {
     return `${company}`;
-  } else if ((email)) {
+  } else if (email) {
     return `${email}`;
   } else {
     return "---";
@@ -28,18 +28,17 @@ const formatVersion = (version, isLocked) => {
 };
 
 const formatPlatformIdentification = (type, id) => {
-  ;
-    const truncID = parseInt(id.substring(0,8), 16);
-    return `${type}-${truncID.toString(16)}`.toLowerCase();
+  const truncID = parseInt(id.substring(0, 8), 16);
+  return `${type}-${truncID.toString(16)}`.toLowerCase();
 };
 
 const formatLastSeen = (lastSeen) => {
   if (lastSeen) {
     return moment
-    .duration(moment(lastSeen).diff(moment()))
-    .locale(navigator.language)
-    .humanize(true)
-  } else{
+      .duration(moment(lastSeen).diff(moment()))
+      .locale(navigator.language)
+      .humanize(true);
+  } else {
     return "---";
   }
 };
@@ -47,30 +46,47 @@ const formatLastSeen = (lastSeen) => {
 const formatRow = (initialConnection, lastSeen) => {
   const initialConnectionMoment = moment(initialConnection);
   const lastSeenMoment = moment(lastSeen);
-  const dayDiff = moment().diff(initialConnectionMoment, 'days')
-  const hourDiff = moment().diff(lastSeenMoment, 'hours')
+  const dayDiff = moment().diff(initialConnectionMoment, "days");
+  const hourDiff = moment().diff(lastSeenMoment, "hours");
 
-  console.log("lastSeen: " + lastSeen);
-  console.log("dayDiff: " + dayDiff);
-  console.log("hourDiff: " + hourDiff);
-
-  if ((dayDiff > 1) && ((!lastSeen) || (hourDiff > 24))) {
-    return "table-danger"
-  } else{
+  if (dayDiff > 1 && (!lastSeen || hourDiff > 24)) {
+    return "table-danger";
+  } else {
     return "";
   }
 };
 
-function DeviceTable({ devices }) {
+function DeviceTable({ devices, updateDeviceOwner, updateDeviceLockVersion }) {
   const { t } = useTranslation(["device", "common"]);
 
-  const [selectedDeviceIndex, setSelectedDeviceIndex] = useState(null);
+  const [selectedDevice, setSelectedDevice] = useState(null);
   const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleOnRowClick = (index) => {
-    setSelectedDeviceIndex(index);
+  const handleClose = (device) => {
+    if (device !== null) {
+      updateDeviceOwner(device);
+    }
+    setSelectedDevice(null);
+    setShow(false);
+  };
+
+  const handleEdit = (device) => {
+    let editDevice = { ...device }
+    setSelectedDevice(editDevice);
     setShow(true);
+  };
+
+  const handleOnToggleLock = (device) => {
+    let editDevice = { ...device }
+    let confirmToggleLock = window.confirm(
+      editDevice.lock_version
+        ? t("device:alertMsg.areYouSureUnlockDevice")
+        : t("device:alertMsg.areYouSureLockDevice")
+    );
+    if (confirmToggleLock) {
+      editDevice.lock_version = !editDevice.lock_version;
+      updateDeviceLockVersion(editDevice);
+    }
   };
 
   return (
@@ -93,11 +109,27 @@ function DeviceTable({ devices }) {
         </thead>
         <tbody>
           {devices.map((device, index) => (
-            <tr className={formatRow(device.initial_connection, device.last_seen)} key={device.device_id} onClick={() => handleOnRowClick(index)}>
-              <td> {formatPlatformIdentification(device.platform_type, device.platform_id)} </td>
+            <tr
+              className={formatRow(device.initial_connection, device.last_seen)}
+              key={device.device_id}
+            >
+              <td>
+                {" "}
+                {formatPlatformIdentification(
+                  device.platform_type,
+                  device.platform_id
+                )}{" "}
+              </td>
               <td> {device.platform_type} </td>
               <td> {device.platform_id} </td>
-              <td> {formatOwner(device.company_name, device.contact_name, device.contact_email)}</td>
+              <td>
+                {" "}
+                {formatOwner(
+                  device.company_name,
+                  device.contact_name,
+                  device.contact_email
+                )}
+              </td>
               <td>
                 {moment(device.initial_connection)
                   .locale(navigator.language)
@@ -109,24 +141,27 @@ function DeviceTable({ devices }) {
                   .locale(navigator.language)
                   .humanize(true)}
               </td>
-              <td>
-                {formatLastSeen(device.last_seen)}
-              </td>
-              <td> {device.title} </td>
-              <td> {formatVersion(device.version, device.lock_version)}</td>
+              <td>{formatLastSeen(device.last_seen)}</td>
+              <td>{device.title}</td>
+              <td>{formatVersion(device.version, device.lock_version)}</td>
               <td> {device.build_number} </td>
               <td> {device.connection_count} </td>
+              <td>
+                <Button onClick={() => handleEdit(device)}>
+                  {t("common:buttonLabel.edit")}
+                </Button>
+              </td>
+              <td>
+                <Button onClick={() => handleOnToggleLock(device)}>
+                  {t("device:buttonLabel.unlock")}
+                </Button>
+              </td>
             </tr>
           ))}
         </tbody>
       </Table>
       {show ? (
-        <DeviceForm
-          show={show}
-          handleClose={handleClose}
-          devices={devices}
-          selectedDeviceIndex={selectedDeviceIndex}
-        />
+        <DeviceForm show={show} close={handleClose} device={selectedDevice} />
       ) : null}
     </div>
   );
