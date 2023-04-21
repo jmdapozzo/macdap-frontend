bfcabfa8ca9ec15afe70b1782006f0f4355cd087
Jean-Marc Da Pozzo <jmdapozzo@macdap.com>
2023-04-21 18:16:25 -0400

    Feature: better map handling, initial region and markers

--- src/components/map-page.jsx
+++ src/components/map-page.jsx
@@ -1,3 +1,4 @@
+import { useState, useCallback, useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { Container } from "react-bootstrap";
 import { Map, Marker } from "mapkit-react";
@@ -5,14 +6,87 @@ import { Map, Marker } from "mapkit-react";
 const MapPage = () => {
   const { t } = useTranslation(["map"]);
 
+  const [currentPosition, setCurrentPosition] = useState({});
+  const currentPositionSuccess = (position) => {
+    const currentPosition = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    };
+    setCurrentPosition(currentPosition);
+  };
+
+  const currentPositionError = (error) => {
+    const currentPosition = {
+      lat: 45.462504,
+      lng: -73.554223,
+    };
+    setCurrentPosition(currentPosition);
+  };
+
+  useMemo(() => {
+    navigator.geolocation.getCurrentPosition(
+      currentPositionSuccess,
+      currentPositionError
+    );
+  }, []);
+
+  const [markerPositions, setMarkerPositions] = useState([]);
+
+  const onMapClick = useCallback((event) => {
+    setMarkerPositions((current) => [
+      ...current,
+      {
+        type: "xxx",
+        coord: event.toCoordinates(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  let region = {
+    centerLatitude: 45.5019,
+    centerLongitude: -73.5674,
+    latitudeDelta: 1.0,
+    longitudeDelta: 1.0,
+  };
+
   return (
     <Container className="vh-100">
       <Map
         token={process.env.REACT_APP_APPLE_MAP_JWT}
         width="100px"
         height="100px"
+        showsUserLocation="true"
+        showsUserLocationControl="true"
+        onClick={onMapClick}
+        initialRegion={region}
       >
-        <Marker latitude={46.52} longitude={6.57} />
+        {markerPositions.map((markerPosition) => {
+          let marker;
+          switch (markerPosition.type) {
+            case "xxx":
+              marker = (
+                <Marker
+                  key={markerPosition.time.toISOString()}
+                  latitude={markerPosition.coord.latitude}
+                  longitude={markerPosition.coord.longitude}
+                />
+              );
+              break;
+
+            default:
+              marker = marker = (
+                <Marker
+                  key={markerPosition.time.toISOString()}
+                  latitude={markerPosition.coord.latitude}
+                  longitude={markerPosition.coord.longitude}
+                />
+              );
+          }
+          return marker;
+        })}
+
+        {/* <Marker latitude={45.449114214544856} longitude={-73.55212640146412} /> */}
       </Map>
     </Container>
   );
