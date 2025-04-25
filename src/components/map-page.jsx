75777e037066ca1b4d8f7dd18f6b969edf6b543d
jmdapozzo <jmdapozzo@macdap.com>
2025-04-25 15:22:19 -0400

    Fixed map page

--- src/components/map-page.jsx
+++ src/components/map-page.jsx
@@ -1,39 +1,19 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
+// import { useKeycloak } from "@react-keycloak/web";
 import { Container } from "react-bootstrap";
 import { Map, Marker } from "mapkit-react";
+import Loading from "./loading";
 
 // voir https://rajeshlv.medium.com/using-apple-mapkit-js-for-web-applications-15c4cff0e2
 // voir exemple de Apple avec génération token por Node https://developer.apple.com/documentation/mapkitjs/mapkit/init
 
 const MapPage = () => {
   const { t } = useTranslation(["map"]);
-/*
-  const [currentPosition, setCurrentPosition] = useState({});
-  const currentPositionSuccess = (position) => {
-    const currentPosition = {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    };
-    setCurrentPosition(currentPosition);
-  };
-
-  const currentPositionError = (error) => {
-    const currentPosition = {
-      lat: 45.462504,
-      lng: -73.554223,
-    };
-    setCurrentPosition(currentPosition);
-  };
-
-  useMemo(() => {
-    navigator.geolocation.getCurrentPosition(
-      currentPositionSuccess,
-      currentPositionError
-    );
-  }, []);
-*/
+  // const { keycloak } = useKeycloak();
   const [markerPositions, setMarkerPositions] = useState([]);
+  const [mapToken, setMapToken] = useState(null);
+  const [error, setError] = useState(null);
 
   const onMapClick = useCallback((event) => {
     setMarkerPositions((current) => [
@@ -53,10 +33,65 @@ const MapPage = () => {
     longitudeDelta: 1.0,
   };
 
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_SERVER_ENDPOINT}/mapjwt/xx`);
+        if (!response.ok) {
+          throw new Error("Failed to fetch the map token");
+        }
+        const data = await response.json();
+        console.log("---------------- map token", data.token);
+        setMapToken(data.token);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchToken();
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchToken = async () => {
+  //     try {
+
+  //       const token = keycloak.token;
+  //       console.log("---------------- keycloak token", token);
+  //       const response = await fetch(
+  //         process.env.REACT_APP_SERVER_ENDPOINT + "/mapjwt/xx",
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${token}`,
+  //           },
+  //         }
+  //       );
+
+  //       if (!response.ok) {
+  //         throw new Error("Failed to fetch the map token");
+  //       }
+  //       const data = await response.json();
+  //       console.log("---------------- map token", token);
+  //       setMapToken(data.token);
+  //     } catch (err) {
+  //       setError(err.message);
+  //     }
+  //   };
+
+  //   fetchToken();
+  // }, [keycloak]);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!mapToken) {
+    return <Loading />;
+  }
+
   return (
     <Container className="vh-100">
       <Map
-        token={process.env.REACT_APP_APPLE_MAP_JWT}
+        token={mapToken}
         width="100px"
         height="100px"
         title={t("title")}
@@ -79,7 +114,7 @@ const MapPage = () => {
               break;
 
             default:
-              marker = marker = (
+              marker = (
                 <Marker
                   key={markerPosition.time.toISOString()}
                   latitude={markerPosition.coord.latitude}
@@ -89,8 +124,6 @@ const MapPage = () => {
           }
           return marker;
         })}
-
-        {/* <Marker latitude={45.449114214544856} longitude={-73.55212640146412} /> */}
       </Map>
     </Container>
   );
